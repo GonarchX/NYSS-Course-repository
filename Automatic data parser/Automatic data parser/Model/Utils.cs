d580f0ddc29dd99b175c35530ad3222e4863347a
GonarchX <valentinpronin@mail.ru>
2021-12-01 15:46:15 +0300

    Refactoring + added sync data feature

--- Automatic data parser/Automatic data parser/Model/Utils.cs
+++ Automatic data parser/Automatic data parser/Model/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using LinqToExcel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -19,13 +20,12 @@ namespace Automatic_data_parser.Model
         /// <returns>Parsed information from row as ThreatInfoModel</returns>
         public static ThreatInfoModel ParseInfoFromRow(LinqToExcel.Row row)
         {
-            int threatID;
-            if (!Int32.TryParse(Convert.ToString(row[0].Value), out threatID))
+            if (!Int32.TryParse(Convert.ToString(row[0].Value), out int threatID))
             {
                 throw new ArgumentException("Failed to get data from file\n" +
                     "Cause: Invalid value in the column (required value: integer)\n" +
                     $"Current value: {Convert.ToString(row[0].Value)}\n" +
-                    "Column: Threat ID\n");
+                    "Column: Threat ID");
             }
             string threatName = Convert.ToString(row[1].Value);
             string descriptionOfTheThreat = Convert.ToString(row[2].Value);
@@ -44,7 +44,7 @@ namespace Automatic_data_parser.Model
                 breachOfConfidentiality = Convert.ToString(row[5].Value) == "1";
             }
 
-            bool integrityViolation = false;
+            bool integrityViolation;
             if (Convert.ToString(row[6].Value) != "0" && Convert.ToString(row[6].Value) != "1")
             {
                 throw new ArgumentException("Failed to get data from file\n" +
@@ -57,7 +57,7 @@ namespace Automatic_data_parser.Model
                 integrityViolation = Convert.ToString(row[6].Value) == "1";
             }
 
-            bool accessibilityViolation = false;
+            bool accessibilityViolation;
             if (Convert.ToString(row[7].Value) != "0" && Convert.ToString(row[7].Value) != "1")
             {
                 throw new ArgumentException("Failed to get data from file\n" +
@@ -80,45 +80,46 @@ namespace Automatic_data_parser.Model
                                   accessibilityViolation);
         }
 
-        public static string CheckFileData(string filePath)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void SaveDataGridDataToFile(IList<ThreatInfoModel> dataInDataGrid, string fileName)
+        public static void SaveThreatDataToFile(IList<ThreatInfoModel> dataInDataGrid, int numberOfColumns, string fileName)
         {
             string fullPath = Directory.GetCurrentDirectory() + $@"\{fileName}";
 
             _Excel.Application excel = new _Excel.Application();
             _Excel.Workbook workbook = excel.Workbooks.Open(fullPath);
             _Excel.Worksheet worksheet = workbook.Worksheets[1];
-
+             
             // Starting row is 3 because 1 and 2 rows for column names
             int startRow = 3;
-            for (int i = 0; i < dataInDataGrid.Count(); i++)
+
+            object[,] arr = new object[dataInDataGrid.Count, numberOfColumns];
+            for (int i = 0; i < dataInDataGrid.Count; i++)
             {
-                worksheet.Cells[i + startRow, 1].Value = dataInDataGrid[i].ThreatID;
-                worksheet.Cells[i + startRow, 2].Value = dataInDataGrid[i].ThreatName;
-                worksheet.Cells[i + startRow, 3].Value = dataInDataGrid[i].DescriptionOfTheThreat;
-                worksheet.Cells[i + startRow, 4].Value = dataInDataGrid[i].TheSourceOfTheThreat;
-                worksheet.Cells[i + startRow, 5].Value = dataInDataGrid[i].ThreatImpactObject;
-                worksheet.Cells[i + startRow, 6].Value = dataInDataGrid[i].BreachOfConfidentiality == true ? "1" : "0";
-                worksheet.Cells[i + startRow, 7].Value = dataInDataGrid[i].IntegrityViolation == true ? "1" : "0";
-                worksheet.Cells[i + startRow, 8].Value = dataInDataGrid[i].AccessibilityViolation == true ? "1" : "0";
-                worksheet.Rows[i + startRow].RowHeight = 15;
+                arr[i, 0] = dataInDataGrid[i].ThreatID;
+                arr[i, 1] = dataInDataGrid[i].ThreatName;
+                arr[i, 2] = dataInDataGrid[i].DescriptionOfTheThreat;
+                arr[i, 3] = dataInDataGrid[i].TheSourceOfTheThreat;
+                arr[i, 4] = dataInDataGrid[i].ThreatImpactObject;
+                arr[i, 5] = dataInDataGrid[i].BreachOfConfidentiality == true ? "1" : "0";
+                arr[i, 6] = dataInDataGrid[i].IntegrityViolation == true ? "1" : "0";
+                arr[i, 7] = dataInDataGrid[i].AccessibilityViolation == true ? "1" : "0";
             }
 
-            workbook.SaveAs(fullPath, _Excel.XlFileFormat.xlWorkbookDefault, null, null);
-            workbook.Close();
-            excel.Quit();
+            _Excel.Range range = worksheet.Range[worksheet.Cells[startRow, 1], worksheet.Cells[dataInDataGrid.Count, numberOfColumns]];
+
+            range.Value = arr;
+
+            worksheet.Rows.RowHeight = 15;
+            workbook.SaveAs(fullPath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, _Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing);
+            workbook.Close(true);
+            excel.Quit();            
         }
 
-        public static ExcelQueryFactory GetExcelFromFile(string filePath)
+        public static ExcelQueryFactory GetExcelFromFile(string fileName)
         {
-            if (filePath.Length == 0) throw new ArgumentException("File path should not be empty!");
-            else if (filePath is null) throw new ArgumentNullException("File path should not be null!");
-            else if (!File.Exists(filePath)) throw new FileNotFoundException("Could not find the file in the specified directory!");
-            return new ExcelQueryFactory(filePath);
+            if (fileName.Length == 0) throw new ArgumentException("File name should not be empty!");
+            else if (fileName is null) throw new ArgumentNullException("File name should not be null!");
+            else if (!File.Exists(fileName)) throw new FileNotFoundException($"Could not find the file \"{fileName}\" in the project directory!");
+            return new ExcelQueryFactory(fileName);
         }
 
         /// <summary>
@@ -144,7 +145,8 @@ namespace Automatic_data_parser.Model
                 {
                     threatData = null;
                     // Row = i + 2 because 2 rows in Excel table are rows for column names
-                    throw new ArgumentException(exc.Message + $"\nRow: {i + 2}");
+                    throw new ArgumentException(exc.Message + $"\nRow: {i + 2}\n" +
+                        $"File name: {excelFile.FileName}");
                 }
                 i++;
             }
@@ -162,6 +164,47 @@ namespace Automatic_data_parser.Model
             using (WebClient client = new WebClient())
             {
                 client.DownloadFile(url, fileName);
+            }
+        }
+
+        public static bool IsDifferenceRows(ThreatInfoModel firstRow, ThreatInfoModel secondRow)
+        {
+            if (firstRow.ThreatID != secondRow.ThreatID) { return true; }
+            else if (firstRow.ThreatName != secondRow.ThreatName) { return true; }
+            else if (firstRow.DescriptionOfTheThreat != secondRow.DescriptionOfTheThreat) { return true; }
+            else if (firstRow.TheSourceOfTheThreat != secondRow.TheSourceOfTheThreat) { return true; }
+            else if (firstRow.ThreatImpactObject != secondRow.ThreatImpactObject) { return true; }
+            else if (firstRow.BreachOfConfidentiality != secondRow.BreachOfConfidentiality) { return true; }
+            else if (firstRow.IntegrityViolation != secondRow.IntegrityViolation) { return true; }
+            else if (firstRow.AccessibilityViolation != secondRow.AccessibilityViolation) { return true; }
+
+            return false;
+        }
+
+        public static void GetDifferenceRows (IList<ThreatInfoModel> firstSourceData, IList<ThreatInfoModel> secondSourceData, out IList<ThreatInfoModel> firstDifferenceData, out IList<ThreatInfoModel> secondDifferenceData)
+        {
+            var firstSourceDataIDs = firstSourceData.Select(x => x.ThreatID).ToList();
+            var secondSourceDataIDs = secondSourceData.Select(x => x.ThreatID).ToList();
+            firstDifferenceData = new List<ThreatInfoModel>();
+            secondDifferenceData = new List<ThreatInfoModel>();
+
+            foreach (var currentThreatID in secondSourceDataIDs)
+            {
+                if (!firstSourceDataIDs.Contains(currentThreatID))
+                {
+                    secondDifferenceData.Add(secondSourceData.First(x => x.ThreatID == currentThreatID));
+                    firstDifferenceData.Add(new ThreatInfoModel(secondDifferenceData.Last().ThreatID, "There was no such threat", "There was no such threat", "There was no such threat", "There was no such threat", false, false, false));
+                }
+                else
+                {
+                    var curLocalThreat = firstSourceData.First(x => x.ThreatID == currentThreatID);
+                    var curLoadedThreat = secondSourceData.First(x => x.ThreatID == currentThreatID);
+                    if (Utils.IsDifferenceRows(curLocalThreat, curLoadedThreat))
+                    {
+                        firstDifferenceData.Add(firstSourceData.First(x => x.ThreatID == currentThreatID));
+                        secondDifferenceData.Add(secondSourceData.First(x => x.ThreatID == currentThreatID));
+                    }
+                }
             }
         }
     }
